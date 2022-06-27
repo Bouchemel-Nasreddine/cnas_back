185c2d4d25c2dccfcf883663012d67691fecabe6
Nasreddine Bouchemel <kn_bouchemel@esi.dz>
2022-06-27 18:17:55 +0100

    headers3

--- index.js
+++ index.js
@@ -84,11 +84,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(function(req, res, next) {
+  console.log(req);
   res.setTimeout(200);
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST ,PUT, OPTIONS, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Max-Age', '86400')
+  res.header('Access-Control-Max-Age', '86400');
   next();
 });
 
