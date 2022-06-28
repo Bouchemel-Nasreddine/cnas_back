10f0162fbbbbab4bf3a5c1533e3d814a2677b70d
Nasreddine Bouchemel <kn_bouchemel@esi.dz>
2022-06-28 16:20:18 +0100

    demande avec patient5

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ app.get('/demande/:id', function(req, res)  {
     if(rows.length != 0){
                   data = rows[0];
                   console.log(data["id_patient"]);
-                  connection.query("select * from patient where '"+data['id_patient']+"' = patient.id_patient;", (error, results, fields) =>{
+                  connection.query("select * from patient wheree '"+data['id_patient']+"' = patient.id_patient;", (error, results, fields) =>{
                     if (error) throw error
                     if (results.length != 0) {
                       patient = results[0];
